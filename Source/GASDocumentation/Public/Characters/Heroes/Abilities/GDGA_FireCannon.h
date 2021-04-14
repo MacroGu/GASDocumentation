7b206a56976746c0a3e4173b10adc71d2d41877d
macro gu <macrogu@qq.com>
2021-04-14 11:18:10 +0800

    添加大炮技能 按键G 为触发 是 ability6 技能

---
+++ Source/GASDocumentation/Public/Characters/Heroes/Abilities/GDGA_FireCannon.h
@@ -0,0 +1,17 @@
+// Copyright 2020 Dan Kestranek.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/Abilities/GDGameplayAbility.h"
+#include "GDGA_FireCannon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GASDOCUMENTATION_API UGDGA_FireCannon : public UGDGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
